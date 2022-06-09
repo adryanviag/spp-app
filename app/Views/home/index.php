1ece4f07820b186fa7bfa8b6ec5c504723319235
WHITE-WOLF <adryanviag@gmail.com>
2022-06-09 19:40:54 +0700

    first commit

---
+++ app/Views/home/index.php
@@ -0,0 +1,28 @@
+<?= $this->extend('layout/index.php') ?>
+
+<?= $this->section('content') ?>
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800"><?= $title ?></h1>
+</div>
+
+<!-- Content Row -->
+<div class="row">
+    <!-- Content Column -->
+    <div class="col-lg-6 mb-4">
+
+    </div>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<?= $this->endSection() ?>
